187005c2e395e11287a6e40ce513c520d7c0f50e
x-cold <lxstart@outlook.com>
2018-08-13 17:22:19 +0800

    chore: add basic config file
    Change-Id: I52b26a143655ffa68fdbe4d4003ac193705c10d6

--- config/config.default.js
+++ config/config.default.js
@@ -1,20 +1,50 @@
 'use strict';
 
 const path = require('path');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-module.exports = {
-  keys: 'key',
-  static: {
-    maxAge: 0,
-  },
-  isomorphic: {
-    babel: {
-      plugins: [require.resolve('babel-plugin-transform-decorators-legacy')],
+let blogConfig = {};
+
+// Get document, or throw exception on error
+try {
+  blogConfig = yaml.safeLoad(
+    fs.readFileSync(
+      path.join(__dirname, './config.yml'),
+      'utf8'
+    )
+  );
+} catch (e) {
+  console.log(e);
+}
+
+module.exports = (appInfo) => {
+  const clientViewRoot = path.join(appInfo.baseDir, `themes/${blogConfig.theme}`);
+  const serverViewRoot = path.join(appInfo.baseDir, '/app/view');
+  return {
+    keys: 'key',
+    static: {
+      maxAge: 0,
     },
-  },
-  webpack: {
-    custom: {
-      configPath: path.join(__dirname, './webpack.config.js'),
+    client: clientViewRoot,
+    view: {
+      root: `$${serverViewRoot},${clientViewRoot}`,
     },
-  },
+    isomorphic: {
+      babel: {
+        plugins: [
+          require.resolve('babel-plugin-transform-decorators-legacy'),
+        ],
+      },
+      alias: {
+        client: clientViewRoot,
+      },
+    },
+    webpack: {
+      custom: {
+        configPath: path.join(appInfo.baseDir, 'config/webpack.config.js'),
+      },
+    },
+    blog: blogConfig,
+  };
 };
